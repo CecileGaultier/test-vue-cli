c9609b10f2e88b982f224527d10fbc4357f4c2f6
CecileGaultier <70898757+CecileGaultier@users.noreply.github.com>
2020-11-04 19:46:44 +0100

    Commit : transition pour SimpleDialog

--- src/SimpleDialog.vue
+++ src/SimpleDialog.vue
@@ -1,17 +1,23 @@
 <template>
     <div class="dialog-parent">
-        <div class="dialog-backdrop" v-show="visible"></div>
-        <div class="dialog-window" v-show="visible">
-            <header>
-                <h1>{{titre}}</h1>
-            </header>
-            <div class="main">
-                <slot/>
+        <transition name="fade">
+            <div class="dialog-backdrop" v-show="visible"></div>
+        </transition>
+
+        <transition name="zoom">
+            <div class="dialog-window" v-show="visible">
+                <header>
+                    <h1>{{titre}}</h1>
+                </header>
+                <div class="main">
+                    <slot/>
+                </div>
+                <footer>
+                    <button @click="visible = false" v-if="show">Close</button>
+                </footer>
             </div>
-            <footer>
-                <button @click="visible=false">Close</button>
-            </footer>
-        </div>
+        </transition>
+
 
     </div>
 </template>
@@ -40,12 +46,14 @@
             }
         }
     }
+
 </script>
 
 <style scoped>
     .dialog-window {
         position: relative;
         pointer-events: auto;
+        transform: scale(2);
     }
     .dialog-parent {
         pointer-events: none;
@@ -60,10 +68,55 @@
         left: 0;
         width: 100vw;
         height: 100vh;
+
     }
     .dialog-backdrop {
         pointer-events: auto;
         background-color: rgba(255, 30, 80, 0.5);
     }
+
+    .fade-enter-active{
+        animation: fade 2s reverse;
+    }
+
+    .fade-leave-active{
+        animation: fade 2s;
+    }
+
+    .zoom-enter-active{
+        animation: zoom 2s ;
+
+    }
+
+    .zoom-leave-active{
+        animation: zoom 2s reverse;
+    }
+
+    @keyframes fade{
+        0% {
+            opacity: 100%;
+        }
+        50% {
+            opacity: 50%;
+        }
+        100% {
+            opacity: 0;
+        }
+    }
+
+    @keyframes zoom {
+        0% {
+            transform: scale(0);
+        }
+        50% {
+            transform: scale(3);
+        }
+        100% {
+            transform: scale(2);
+        }
+
+    }
+
+
 
 </style>
