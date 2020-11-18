51ddbb1d822f8a8fb56844b371da7d8f9f2b50b1
CecileGaultier <70898757+CecileGaultier@users.noreply.github.com>
2020-11-18 17:06:21 +0100

    usage d'une variable pour changer l'image affichée dans SimpleDialog

--- src/components/LightBox.vue
+++ src/components/LightBox.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="light-box">
         <ul>
-            <li><img src="images/thumbnails/animals-2.jpeg" @click="$refs.dialog.show()" data-full-img="images/animals-2.jpeg" alt=""/></li>
-            <li><img src="images/thumbnails/animals-1.jpeg" @click="$refs.dialog.show()" data-full-img="images/animals-1.jpeg" alt=""/></li>
-            <li><img src="images/thumbnails/animals-3.jpeg" @click="$refs.dialog.show()" data-full-img="images/animals-3.jpeg" alt=""/></li>
-            <li><img src="images/thumbnails/animals-4.jpeg" @click="$refs.dialog.show()" data-full-img="images/animals-4.jpeg" alt=""/></li>
-            <li><img src="images/thumbnails/animals-5.jpeg" @click="$refs.dialog.show()" data-full-img="images/animals-5.jpeg" alt=""/></li>
-            <li><img src="images/thumbnails/animals-6.jpeg" @click="$refs.dialog.show()" data-full-img="images/animals-6.jpeg" alt=""/></li>
-            <li><img src="images/thumbnails/animals-7.jpeg" @click="$refs.dialog.show()" data-full-img="images/animals-7.jpeg" alt=""/></li>
-            <li><img src="images/thumbnails/animals-8.jpeg" @click="$refs.dialog.show()" data-full-img="images/animals-8.jpeg" alt=""/></li>
-            <li><img src="images/thumbnails/animals-9.jpeg" @click="$refs.dialog.show()" data-full-img="images/animals-9.jpeg" alt=""/></li>
+            <li><img src="images/thumbnails/animals-2.jpeg" @click="$refs.dialog.show(); imageCourante='images/animals-2.jpeg'" alt=""/></li>
+            <li><img src="images/thumbnails/animals-1.jpeg" @click="$refs.dialog.show(); imageCourante='images/animals-1.jpeg'" alt=""/></li>
+            <li><img src="images/thumbnails/animals-3.jpeg" @click="$refs.dialog.show(); imageCourante='images/animals-3.jpeg'" alt=""/></li>
+            <li><img src="images/thumbnails/animals-4.jpeg" @click="$refs.dialog.show(); imageCourante='images/animals-4.jpeg'" alt=""/></li>
+            <li><img src="images/thumbnails/animals-5.jpeg" @click="$refs.dialog.show(); imageCourante='images/animals-5.jpeg'" alt=""/></li>
+            <li><img src="images/thumbnails/animals-6.jpeg" @click="$refs.dialog.show(); imageCourante='images/animals-6.jpeg'" alt=""/></li>
+            <li><img src="images/thumbnails/animals-7.jpeg" @click="$refs.dialog.show(); imageCourante='images/animals-7.jpeg'" alt=""/></li>
+            <li><img src="images/thumbnails/animals-8.jpeg" @click="$refs.dialog.show(); imageCourante='images/animals-8.jpeg'" alt=""/></li>
+            <li><img src="images/thumbnails/animals-9.jpeg" @click="$refs.dialog.show(); imageCourante='images/animals-9.jpeg'" alt=""/></li>
         </ul>
 
         <SimpleDialog ref="dialog"
                       titre="Le titre de cette instance">
-            <img src="images/animals-1.jpeg" alt=""
+            <img :src="imageCourante" alt=""
                  width="400" height="200">
             <!-- On met la taille pour ne pas avoir à écrire
                   un code qui attend le chargement de l'image -->
@@ -31,6 +31,12 @@
     export default {
         name: "LightBox",
         components: {SimpleDialog},
+
+    data: function () {
+        return{
+            imageCourante: "",
+        }
+    }
     }
 </script>
 
