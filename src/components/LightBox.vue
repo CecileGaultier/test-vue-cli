3a99646c932a1843a68ec61f13aca41f26c0f4f0
CecileGaultier <70898757+CecileGaultier@users.noreply.github.com>
2020-11-18 17:00:32 +0100

    miniature bouton

--- src/components/LightBox.vue
+++ src/components/LightBox.vue
@@ -1,18 +1,16 @@
 <template>
     <div class="light-box">
         <ul>
-            <li><img src="images/thumbnails/animals-1.jpeg" data-full-img="images/animals-1.jpeg" alt=""/></li>
-            <li><img src="images/thumbnails/animals-2.jpeg" data-full-img="images/animals-2.jpeg" alt=""/></li>
-            <li><img src="images/thumbnails/animals-3.jpeg" data-full-img="images/animals-3.jpeg" alt=""/></li>
-            <li><img src="images/thumbnails/animals-4.jpeg" data-full-img="images/animals-4.jpeg" alt=""/></li>
-            <li><img src="images/thumbnails/animals-5.jpeg" data-full-img="images/animals-5.jpeg" alt=""/></li>
-            <li><img src="images/thumbnails/animals-6.jpeg" data-full-img="images/animals-6.jpeg" alt=""/></li>
-            <li><img src="images/thumbnails/animals-7.jpeg" data-full-img="images/animals-7.jpeg" alt=""/></li>
-            <li><img src="images/thumbnails/animals-8.jpeg" data-full-img="images/animals-8.jpeg" alt=""/></li>
-            <li><img src="images/thumbnails/animals-9.jpeg" data-full-img="images/animals-9.jpeg" alt=""/></li>
+            <li><img src="images/thumbnails/animals-2.jpeg" @click="$refs.dialog.show()" data-full-img="images/animals-2.jpeg" alt=""/></li>
+            <li><img src="images/thumbnails/animals-1.jpeg" @click="$refs.dialog.show()" data-full-img="images/animals-1.jpeg" alt=""/></li>
+            <li><img src="images/thumbnails/animals-3.jpeg" @click="$refs.dialog.show()" data-full-img="images/animals-3.jpeg" alt=""/></li>
+            <li><img src="images/thumbnails/animals-4.jpeg" @click="$refs.dialog.show()" data-full-img="images/animals-4.jpeg" alt=""/></li>
+            <li><img src="images/thumbnails/animals-5.jpeg" @click="$refs.dialog.show()" data-full-img="images/animals-5.jpeg" alt=""/></li>
+            <li><img src="images/thumbnails/animals-6.jpeg" @click="$refs.dialog.show()" data-full-img="images/animals-6.jpeg" alt=""/></li>
+            <li><img src="images/thumbnails/animals-7.jpeg" @click="$refs.dialog.show()" data-full-img="images/animals-7.jpeg" alt=""/></li>
+            <li><img src="images/thumbnails/animals-8.jpeg" @click="$refs.dialog.show()" data-full-img="images/animals-8.jpeg" alt=""/></li>
+            <li><img src="images/thumbnails/animals-9.jpeg" @click="$refs.dialog.show()" data-full-img="images/animals-9.jpeg" alt=""/></li>
         </ul>
-
-        <button @click="$refs.dialog.show()">Open</button>
 
         <SimpleDialog ref="dialog"
                       titre="Le titre de cette instance">
