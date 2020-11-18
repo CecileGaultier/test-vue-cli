0e67c4339412662ba78dbd2ed9082411b7176db3
CecileGaultier <70898757+CecileGaultier@users.noreply.github.com>
2020-11-18 17:24:05 +0100

    chargement des données depuis un fichier JSON

--- src/components/LightBox.vue
+++ src/components/LightBox.vue
@@ -28,46 +28,14 @@
     data: function () {
         return{
             imageCourante: "",
-            imageData:[
-                {
-                    "src": "images/thumbnails/animals-1.jpeg",
-                    "dataFullImg": "images/animals-1.jpeg"
-                },
-                {
-                    "src": "images/thumbnails/animals-2.jpeg",
-                    "dataFullImg": "images/animals-2.jpeg"
-                },
-                {
-                    "src": "images/thumbnails/animals-3.jpeg",
-                    "dataFullImg": "images/animals-3.jpeg"
-                },
-                {
-                    "src": "images/thumbnails/animals-4.jpeg",
-                    "dataFullImg": "images/animals-4.jpeg"
-                },
-                {
-                    "src": "images/thumbnails/animals-5.jpeg",
-                    "dataFullImg": "images/animals-5.jpeg"
-                },
-                {
-                    "src": "images/thumbnails/animals-6.jpeg",
-                    "dataFullImg": "images/animals-6.jpeg"
-                },
-                {
-                    "src": "images/thumbnails/animals-7.jpeg",
-                    "dataFullImg": "images/animals-7.jpeg"
-                },
-                {
-                    "src": "images/thumbnails/animals-8.jpeg",
-                    "dataFullImg": "images/animals-8.jpeg"
-                },
-                {
-                    "src": "images/thumbnails/animals-9.jpeg",
-                    "dataFullImg": "images/animals-9.jpeg"
-                }
-            ],
+            imageData:[],
         }
     },
+        created(){
+            fetch("images-data.json")
+                .then(rep =>rep.json())
+                .then(json=>this.imageData =json)
+        },
 
     methods:{
         afficheImg(url){
