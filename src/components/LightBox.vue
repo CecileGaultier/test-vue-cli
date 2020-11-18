7f3914fe0d798c513c0c13c096afc94ae336c85e
CecileGaultier <70898757+CecileGaultier@users.noreply.github.com>
2020-11-18 17:09:42 +0100

    fonction afficheImg

--- src/components/LightBox.vue
+++ src/components/LightBox.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="light-box">
         <ul>
-            <li><img src="images/thumbnails/animals-2.jpeg" @click="$refs.dialog.show(); imageCourante='images/animals-2.jpeg'" alt=""/></li>
-            <li><img src="images/thumbnails/animals-1.jpeg" @click="$refs.dialog.show(); imageCourante='images/animals-1.jpeg'" alt=""/></li>
-            <li><img src="images/thumbnails/animals-3.jpeg" @click="$refs.dialog.show(); imageCourante='images/animals-3.jpeg'" alt=""/></li>
-            <li><img src="images/thumbnails/animals-4.jpeg" @click="$refs.dialog.show(); imageCourante='images/animals-4.jpeg'" alt=""/></li>
-            <li><img src="images/thumbnails/animals-5.jpeg" @click="$refs.dialog.show(); imageCourante='images/animals-5.jpeg'" alt=""/></li>
-            <li><img src="images/thumbnails/animals-6.jpeg" @click="$refs.dialog.show(); imageCourante='images/animals-6.jpeg'" alt=""/></li>
-            <li><img src="images/thumbnails/animals-7.jpeg" @click="$refs.dialog.show(); imageCourante='images/animals-7.jpeg'" alt=""/></li>
-            <li><img src="images/thumbnails/animals-8.jpeg" @click="$refs.dialog.show(); imageCourante='images/animals-8.jpeg'" alt=""/></li>
-            <li><img src="images/thumbnails/animals-9.jpeg" @click="$refs.dialog.show(); imageCourante='images/animals-9.jpeg'" alt=""/></li>
+            <li><img src="images/thumbnails/animals-2.jpeg" @click="afficheImg('images/animals-2.jpeg')" alt=""/></li>
+            <li><img src="images/thumbnails/animals-1.jpeg" @click="afficheImg('images/animals-1.jpeg')" alt=""/></li>
+            <li><img src="images/thumbnails/animals-3.jpeg" @click="afficheImg('images/animals-3.jpeg')" alt=""/></li>
+            <li><img src="images/thumbnails/animals-4.jpeg" @click="afficheImg('images/animals-4.jpeg')" alt=""/></li>
+            <li><img src="images/thumbnails/animals-5.jpeg" @click="afficheImg('images/animals-5.jpeg')" alt=""/></li>
+            <li><img src="images/thumbnails/animals-6.jpeg" @click="afficheImg('images/animals-6.jpeg')" alt=""/></li>
+            <li><img src="images/thumbnails/animals-7.jpeg" @click="afficheImg('images/animals-7.jpeg')" alt=""/></li>
+            <li><img src="images/thumbnails/animals-8.jpeg" @click="afficheImg('images/animals-8.jpeg')" alt=""/></li>
+            <li><img src="images/thumbnails/animals-9.jpeg" @click="afficheImg('images/animals-9.jpeg')" alt=""/></li>
         </ul>
 
         <SimpleDialog ref="dialog"
@@ -35,6 +35,13 @@
     data: function () {
         return{
             imageCourante: "",
+        }
+    },
+
+    methods:{
+        afficheImg(url){
+            this.imageCourante = url;
+            this.$refs.dialog.show();
         }
     }
     }
