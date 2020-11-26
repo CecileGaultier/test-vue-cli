4c2caa2d5de3078e719e1e9893b7b71ab225b43c
CecileGaultier <70898757+CecileGaultier@users.noreply.github.com>
2020-11-26 14:56:11 +0100

    ajout filtre sur type==="sauvage"

--- src/App.vue
+++ src/App.vue
@@ -16,58 +16,67 @@
   components: {LightBox},
   data: function () {
     return{
-      imageData:[]
+      imageData:[
+          {
+          "src": "images/thumbnails/animals-1.jpeg",
+          "dataFullImg": "images/animals-1.jpeg",
+          "title": "gorille",
+              "type": "sauvage",
+            },
+          {
+              "src": "images/thumbnails/animals-2.jpeg",
+              "dataFullImg": "images/animals-2.jpeg",
+              "title": "biche",
+              "type": "sauvage",
+
+          },
+          {
+              "src": "images/thumbnails/animals-3.jpeg",
+              "dataFullImg": "images/animals-3.jpeg",
+              "title": "cochon",
+              "type": "domestique",
+          },
+          {
+              "src": "images/thumbnails/animals-4.jpeg",
+              "dataFullImg": "images/animals-4.jpeg",
+              "title": "rhino",
+              "type": "sauvage",
+          },
+          {
+              "src": "images/thumbnails/animals-5.jpeg",
+              "dataFullImg": "images/animals-5.jpeg",
+              "title": "chat",
+              "type": "domestique",
+          },
+          {
+              "src": "images/thumbnails/animals-6.jpeg",
+              "dataFullImg": "images/animals-6.jpeg",
+              "title": "chiot",
+              "type": "domestique",
+          },
+          {
+              "src": "images/thumbnails/animals-7.jpeg",
+              "dataFullImg": "images/animals-7.jpeg",
+              "title": "tigre",
+              "type": "sauvage",
+          },
+          {
+              "src": "images/thumbnails/animals-8.jpeg",
+              "dataFullImg": "images/animals-8.jpeg",
+              "title": "girafe",
+              "type": "sauvage",
+          },
+          {
+              "src": "images/thumbnails/animals-9.jpeg",
+              "dataFullImg": "images/animals-9.jpeg",
+              "title": "chien",
+              "type": "domestique",
+          }]
     }
     },
       computed:{
         imageDataFiltree(){
-            return [
-                {
-                    "src": "images/thumbnails/animals-1.jpeg",
-                    "dataFullImg": "images/animals-1.jpeg",
-                    "title": "gorille"
-                },
-                {
-                    "src": "images/thumbnails/animals-2.jpeg",
-                    "dataFullImg": "images/animals-2.jpeg",
-                    "title": "biche"
-                },
-                {
-                    "src": "images/thumbnails/animals-3.jpeg",
-                    "dataFullImg": "images/animals-3.jpeg",
-                    "title": "cochon"
-                },
-                {
-                    "src": "images/thumbnails/animals-4.jpeg",
-                    "dataFullImg": "images/animals-4.jpeg",
-                    "title": "rhino"
-                },
-                {
-                    "src": "images/thumbnails/animals-5.jpeg",
-                    "dataFullImg": "images/animals-5.jpeg",
-                    "title": "chat"
-                },
-                {
-                    "src": "images/thumbnails/animals-6.jpeg",
-                    "dataFullImg": "images/animals-6.jpeg",
-                    "title": "chiot"
-                },
-                {
-                    "src": "images/thumbnails/animals-7.jpeg",
-                    "dataFullImg": "images/animals-7.jpeg",
-                    "title": "tigre"
-                },
-                {
-                    "src": "images/thumbnails/animals-8.jpeg",
-                    "dataFullImg": "images/animals-8.jpeg",
-                    "title": "girafe"
-                },
-                {
-                    "src": "images/thumbnails/animals-9.jpeg",
-                    "dataFullImg": "images/animals-9.jpeg",
-                    "title": "chien"
-                },
-            this.imageData];
+            return this.imageData.filter(({type})=>type==="sauvage");
         }
       }
 }
