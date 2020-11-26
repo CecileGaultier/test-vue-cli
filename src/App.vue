fbfab7e00a955ed6ae7a49135eae897815af9a15
CecileGaultier <70898757+CecileGaultier@users.noreply.github.com>
2020-11-26 14:47:17 +0100

    introduction de la propriétés calculé 'imagesDataFiltree

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app">
       <h1>Bonjour</h1>
-    <LightBox :image-data="imageData" />
+    <LightBox :image-data="imageDataFiltree" />
 
 
   </div>
@@ -16,56 +16,60 @@
   components: {LightBox},
   data: function () {
     return{
-      imageData:[
-          {
-              "src": "images/thumbnails/animals-1.jpeg",
-              "dataFullImg": "images/animals-1.jpeg",
-              "title": "gorille"
-          },
-          {
-              "src": "images/thumbnails/animals-2.jpeg",
-              "dataFullImg": "images/animals-2.jpeg",
-              "title": "biche"
-          },
-          {
-              "src": "images/thumbnails/animals-3.jpeg",
-              "dataFullImg": "images/animals-3.jpeg",
-              "title": "cochon"
-          },
-          {
-              "src": "images/thumbnails/animals-4.jpeg",
-              "dataFullImg": "images/animals-4.jpeg",
-              "title": "rhino"
-          },
-          {
-              "src": "images/thumbnails/animals-5.jpeg",
-              "dataFullImg": "images/animals-5.jpeg",
-              "title": "chat"
-          },
-          {
-              "src": "images/thumbnails/animals-6.jpeg",
-              "dataFullImg": "images/animals-6.jpeg",
-              "title": "chiot"
-          },
-          {
-              "src": "images/thumbnails/animals-7.jpeg",
-              "dataFullImg": "images/animals-7.jpeg",
-              "title": "tigre"
-          },
-          {
-              "src": "images/thumbnails/animals-8.jpeg",
-              "dataFullImg": "images/animals-8.jpeg",
-              "title": "girafe"
-          },
-          {
-              "src": "images/thumbnails/animals-9.jpeg",
-              "dataFullImg": "images/animals-9.jpeg",
-              "title": "chien"
-          }
-      ]
-
+      imageData:[]
     }
-  },
+    },
+      computed:{
+        imageDataFiltree(){
+            return [
+                {
+                    "src": "images/thumbnails/animals-1.jpeg",
+                    "dataFullImg": "images/animals-1.jpeg",
+                    "title": "gorille"
+                },
+                {
+                    "src": "images/thumbnails/animals-2.jpeg",
+                    "dataFullImg": "images/animals-2.jpeg",
+                    "title": "biche"
+                },
+                {
+                    "src": "images/thumbnails/animals-3.jpeg",
+                    "dataFullImg": "images/animals-3.jpeg",
+                    "title": "cochon"
+                },
+                {
+                    "src": "images/thumbnails/animals-4.jpeg",
+                    "dataFullImg": "images/animals-4.jpeg",
+                    "title": "rhino"
+                },
+                {
+                    "src": "images/thumbnails/animals-5.jpeg",
+                    "dataFullImg": "images/animals-5.jpeg",
+                    "title": "chat"
+                },
+                {
+                    "src": "images/thumbnails/animals-6.jpeg",
+                    "dataFullImg": "images/animals-6.jpeg",
+                    "title": "chiot"
+                },
+                {
+                    "src": "images/thumbnails/animals-7.jpeg",
+                    "dataFullImg": "images/animals-7.jpeg",
+                    "title": "tigre"
+                },
+                {
+                    "src": "images/thumbnails/animals-8.jpeg",
+                    "dataFullImg": "images/animals-8.jpeg",
+                    "title": "girafe"
+                },
+                {
+                    "src": "images/thumbnails/animals-9.jpeg",
+                    "dataFullImg": "images/animals-9.jpeg",
+                    "title": "chien"
+                },
+            this.imageData];
+        }
+      }
 }
 </script>
 
